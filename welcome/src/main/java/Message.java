47c511f8f8c5ab3d85731a448c8200f0bf21277d
TyshchenkoRoma <tyshenko.roma@gmail.com>
2016-09-28 07:49:53 +0300

    created ligic and location

--- welcome/src/main/java/Message.java
+++ welcome/src/main/java/Message.java
@@ -1,5 +1,7 @@
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
+import java.util.ResourceBundle;
 import java.util.logging.Logger;
 
 /**
@@ -9,25 +11,27 @@ public class Message {
     private static final Logger logger = Logger.getLogger("Message");
 
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         Date date = new Date();
         SimpleDateFormat format1 = new SimpleDateFormat("kk");
         double time = Double.parseDouble(format1.format(date));
 
-        if ( time >= 6 && time < 9 ) {
-            System.out.println("Good morning, World!");
+        ResourceBundle resourceBundle = ResourceBundle.getBundle(
+                "Message", Locale.forLanguageTag("en"));
+
+        if (time >= 6 && time < 9) {
+            System.out.println("s1");
         }
-        if ( time >= 9 && time < 19 ) {
-            System.out.println("Good day, World!");
+        if (time >= 9 && time < 19) {
+            System.out.println("s2");
         }
-        if ( time >= 19 && time < 23 ) {
-            System.out.println("Good evening, World!");
+        if (time >= 19 && time < 23) {
+            System.out.println(resourceBundle.getString("s3"));
         }
-        if ( time >= 23 || time < 6 ) {
-            System.out.println("Good night, World! ");
+        if (time >= 23 || time < 6) {
+            System.out.println("s4");
         }
-        System.out.println(time);
 
     }
 }
